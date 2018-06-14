530f233c6f45814a1592f2f24f5185919a9fc2f7
Rohan Singh <rohaniiitv@gmail.com>
2018-06-14 23:24:28 +0530

    working on scrolling-navigation pattern

--- js/index.js
+++ js/index.js
@@ -1,2 +1,53 @@
 var headerSection = document.getElementById("header_section");
 headerSection.style.backgroundImage = "url('img/bg-1.jpg')";
+
+var routes = document.getElementsByClassName("route");
+
+for (var i=0;i<routes.length;i++)
+{
+	routes[i].addEventListener("click",function () {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active","");
+		this.className += " active";
+	});
+}
+
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#header_section a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#header_section nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
